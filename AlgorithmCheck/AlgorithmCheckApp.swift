abef25aed577b6d77fca993dbac7b206f9da93a3
Nikita S <snikitasergeevich@gmail.com>
2023-06-10 15:16:48 +0300

    Initial Commit

---
+++ AlgorithmCheck/AlgorithmCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmCheckApp.swift
+//  AlgorithmCheck
+//
+//  Created by NikitaSergeevich on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
